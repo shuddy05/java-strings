9ef979d0112fe43348ef41d476417f98ff083ca2
shuddy05 <horlabodehyibrahim@gmail.com>
2025-08-11 16:05:56 +0100

    first commit

---
+++ variable.js
@@ -0,0 +1,34 @@
+console.log("HELLO CLASS");
+console.log("IBRAHIM MOSHOOD OLOYEDE");
+// JAVA SCRIPT VARIABLES - x=3 y=2
+// Declare Variables - const, let
+
+const age = 60;
+//age = 78; error
+console.log(age);
+
+let y = 90;
+y = 100;
+console.log(y);
+
+// Rules of naming varabeles
+// 1. variables cannot start with numbers nor %
+// 2. Reserved keywords (const, let if)
+// 3. You cannot have spaces in  variables
+
+// Conventions when naming variables
+// 1. Use logical and meaningful names
+// 2. compound names-- we use camelCasing e.g accountNumber instead of  account number
+const accountNumber = 23678990;
+const myCountry = "Togo";
+//determine if the var is going to const
+
+const firstName = "Ibrahim";
+const countryOfBirth = "Nigeria";
+const minimumEligibleVotingAgeInNig = 18;
+//DATA TYPES
+// Simple (Primitive) & Complex (Non-primitive)
+//SIMPLE DATA TYPES
+// Strings, numbers, null, boolean and undifined
+//COMPLEX DATA TYPES
+// arrays, objects
