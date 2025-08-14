dfdb643e37f16e8968c64ee0e25bf5f44cb7e84f
shuddy05 <horlabodehyibrahim@gmail.com>
2025-08-14 15:47:41 +0100

    new

--- app.js
+++ app.js
@@ -1,99 +1,189 @@
-// Two teams — Team A and Team B — compete in a game three times.
-// Their scores are as follows:
+//JAVA SCRIPT FUNCTION -
+// reusable blocks of code that perfoms a specific task or return a value
+// call or invove the function
 
-// Team A: 96, 108, 89
+// a function that welcomes our user when the log in
 
-// Team B: 88, 91, 110
-
-// Write a JavaScript program to:
-
-// Calculate the average score for each team.
-
-// Determine which team has the higher average score.
-
-// Print the winning team and their average score. If the averages are the same, print "It's a draw!".
-
-// e.g Team B wins with an average score of 96.33
-
-const totalscoreofteamA = 96 + 108 + 89;
-console.log(totalscoreofteamA);
-const totalscoreofteamB = 88 + 91 + 110;
-console.log(totalscoreofteamB);
-const AvgOfA = totalscoreofteamA / 3;
-console.log(AvgOfA);
-const AvgOfB = totalscoreofteamB / 3;
-console.log(AvgOfB);
-if (AvgOfA > AvgOfB) {
-  console.log(`Team A wins with an average score of ${AvgOfA}`);
-} else if (AvgOfB > AvgOfA) {
-  console.log(`Team B wins with an average score of ${AvgOfB}`);
-} else {
-  console.log("Its a Draw");
+function welcomeUser() {
+  console.log("WELCOME USER");
+}
+// calling a function
+welcomeUser();
+welcomeUser();
+// function with parameters - values that are passed to functions
+function welcomeUser2(user) {
+  console.log(`Welcome ${user}`);
 }
 
-//QUESTION 2
-
-// A user is shopping online and has 3 items in their cart with the following prices:
-
-// Item 1: 120
-
-// Item 2: 80
-
-// Item 3: 150
-
-// The user’s account balance is 400.
-
-// Tasks:
-
-// Store the prices of the three items in separate variables.
-
-// Calculate the total cost of all items in the cart.
-
-// Use a conditional statement to determine if the user can proceed to checkout:
-
-// If the balance is greater than or equal to the total cost, print "You can checkout".
-
-// Otherwise, print "Insufficient balance".
-
-const Item1 = 120;
-const Item2 = 80;
-const Item3 = 150;
-const UsersBalance = 400;
-
-const TotalCost = Item1 + Item2 + Item3;
-
-// if (TotalCost > UsersBalance) {
-//   console.log("You can checkout");
-// } else {
-//   console.log("Inufficient Balance");
-// }
-
-UsersBalance >= TotalCost
-  ? console.log("You can checkout")
-  : console.log("Insufficient Balance");
-
-const Movieage = 18;
-const Userage = 20;
-// if (Userage > Movieage) {
-//   console.log("You can watch the movie");
-// } else {
-//   console.log("Your are not old enough");
-// }
-Userage > Movieage
-  ? console.log("You can watch the movie")
-  : console.log("Your are not old enough");
-const ticketPrice = 150;
-const disccount = 30;
-const userB = 120;
-const isFan = false;
-const disccountP = ticketPrice - disccount;
-if (isFan) {
-  console.log(`You are paying${disccountP}`);
-} else {
-  console.log(`You are paying ${ticketPrice}`);
+welcomeUser2("Season");
+welcomeUser2("Daniel");
+welcomeUser2("Ola");
+// deafult parameters
+function welcomeUser3(user = "anonymous") {
+  console.log(`Welcome ${user}`);
 }
-if (userB >= ticketPrice || userB >= disccountP) {
-  console.log("You can buy the football ticket");
-} else {
-  console.log("You do not have enough money for the football ticket");
+welcomeUser3();
+
+// functions returning a value -  giving back a result
+// return keyword
+
+// function that return the additiions of two numbers
+function add(a, b) {
+  return a + b;
 }
+let sum = add(12, 10);
+console.log(sum);
+// function that return the product of two numbers
+function product(numb1, numb2) {
+  return numb1 * numb2;
+}
+console.log(product(65, 3));
+// function that returns the naira equivalent of a dollar amount passed into it
+// 10 dollars will give you (10 * 1600) naira
+// 1$ = 1600
+
+function convertToNaira(dollaramount) {
+  return `${dollaramount} dollars will give you ${dollaramount * 1600} Naira`;
+}
+console.log(convertToNaira(22));
+console.log(convertToNaira(40));
+
+// write a function the returns the average of three numbers
+
+function calAVG(n1, n2, n3) {
+  const avg = (n1, n2, n3) / 3;
+  return avg;
+}
+console.log(calAVG(12, 13, 14));
+
+function Dob(yob) {
+  return 2025 - yob;
+}
+console.log(Dob(2004));
+
+// write  a functiion that return if a nuber is even or odd
+function oddOrEven(num) {
+  if (num % 2 === 0) {
+    return `${num} is EVEN`;
+  } else {
+    return `${num} is ODD`;
+  }
+}
+console.log(oddOrEven(79));
+console.log(oddOrEven(70));
+
+// write a function to check if a withdraw is possible or not
+
+let balance = 2000;
+function withdrawAmount(amount) {
+  if (balance >= amount) {
+    balance -= amount;
+    return "Transaction Successful";
+  } else {
+    return "Insufficient Funds";
+  }
+}
+
+console.log(withdrawAmount(1800));
+console.log(balance);
+console.log(withdrawAmount(1000));
+
+function depositAmount(amount) {
+  balance += amount;
+  return `Deposit of ${amount} is successful, and your current balane ${balance}`;
+}
+
+console.log(depositAmount(3000));
+console.log(withdrawAmount(1000));
+// RETURN KEYWORD
+// any code after return is inaccessible code
+function example() {
+  console.log("okay after return");
+  return "hello";
+  //unreachable code
+}
+
+function firstLetter(letter) {
+  return letter.charAt(0);
+}
+console.log(firstLetter("daniel"));
+
+function getFull(firstname, lastname) {
+  return firstname + " " + lastname;
+}
+console.log(getFull("Daniel", "Season"));
+
+function longestString(String1, String2) {
+  if (String1.length > String2.length) {
+    return String1;
+  } else if (String2.length > String1.length) {
+    return String2;
+  } else {
+    return "Same length";
+  }
+}
+console.log(longestString("ola", "daniel"));
+
+// function espression, treats function like variable
+
+// function exppression that return the square of a num
+const squareNumber = function (num) {
+  return num ** 2;
+};
+console.log(squareNumber(89));
+
+// function expression that converts number of days to weeks
+const ddaysToWeeks = function (days) {
+  return days / 7;
+};
+
+// arrow function (modern)
+// arrow function that returns the difference between two numbers
+
+const difference2 = (n1, n2) => {
+  const diff = n1 - n2;
+  return diff;
+};
+
+// Shorten arrow functions (works only for one line of code inside the block)
+
+const difference = (n1, n2) => n1 - n2;
+console.log(difference(7, 9));
+
+// HOISTING - behaviour ini JS where function declarations are moved to the top before the code is executed
+hoistedFunc(); // this is working because of hoisting
+function hoistedFunc() {
+  console.log("i am a func dec");
+}
+
+const getMaxNum = (num1, numb2) => {
+  if (num1 > numb2) {
+    return num1;
+  } else if (numb2 > num1) {
+    return numb2;
+  } else {
+    return "Equal number";
+  }
+};
+console.log(getMaxNum(6, 7));
+
+const letter = (string) => string.toUpperCase();
+console.log(letter("daniel"));
+let savings = 50000;
+const checkLoanEligibility = (loanAmount) => {
+  if (savings * 2 < loanAmount) {
+    return "User is Inligible";
+  } else {
+    return "User is Eligible";
+  }
+};
+console.log(checkLoanEligibility(50000));
+
+const checkLoanEligibility1 = (loanAmount) =>
+  savings * 2 < loanAmountN ? "User is Inligible" : "User is Eligible";
+
+const containSubstring = (str, substr) => str.includes(substr);
+console.log(containSubstring("daniel", "nie"));
+
+const convertsMtoS = (nthMinute) => nthMinute * 60;
+console.log(convertsMtoS(120));
---
+++ classwork.js
@@ -0,0 +1,160 @@
+// Two teams — Team A and Team B — compete in a game three times.
+// Their scores are as follows:
+
+// Team A: 96, 108, 89
+
+// Team B: 88, 91, 110
+
+// Write a JavaScript program to:
+
+// Calculate the average score for each team.
+
+// Determine which team has the higher average score.
+
+// Print the winning team and their average score. If the averages are the same, print "It's a draw!".
+
+// e.g Team B wins with an average score of 96.33
+
+const totalscoreofteamA = 96 + 108 + 89;
+console.log(totalscoreofteamA);
+const totalscoreofteamB = 88 + 91 + 110;
+console.log(totalscoreofteamB);
+const AvgOfA = totalscoreofteamA / 3;
+console.log(AvgOfA);
+const AvgOfB = totalscoreofteamB / 3;
+console.log(AvgOfB);
+if (AvgOfA > AvgOfB) {
+  console.log(`Team A wins with an average score of ${AvgOfA}`);
+} else if (AvgOfB > AvgOfA) {
+  console.log(`Team B wins with an average score of ${AvgOfB}`);
+} else {
+  console.log("Its a Draw");
+}
+
+//QUESTION 2
+
+// A user is shopping online and has 3 items in their cart with the following prices:
+
+// Item 1: 120
+
+// Item 2: 80
+
+// Item 3: 150
+
+// The user’s account balance is 400.
+
+// Tasks:
+
+// Store the prices of the three items in separate variables.
+
+// Calculate the total cost of all items in the cart.
+
+// Use a conditional statement to determine if the user can proceed to checkout:
+
+// If the balance is greater than or equal to the total cost, print "You can checkout".
+
+// Otherwise, print "Insufficient balance".
+
+const Item1 = 120;
+const Item2 = 80;
+const Item3 = 150;
+const UsersBalance = 400;
+
+const TotalCost = Item1 + Item2 + Item3;
+
+// if (TotalCost > UsersBalance) {
+//   console.log("You can checkout");
+// } else {
+//   console.log("Inufficient Balance");
+// }
+
+UsersBalance >= TotalCost
+  ? console.log("You can checkout")
+  : console.log("Insufficient Balance");
+
+const Movieage = 18;
+const Userage = 20;
+// if (Userage > Movieage) {
+//   console.log("You can watch the movie");
+// } else {
+//   console.log("Your are not old enough");
+// }
+Userage > Movieage
+  ? console.log("You can watch the movie")
+  : console.log("Your are not old enough");
+
+//
+const ticketPrice = 150;
+const disccount = 30;
+const userB = 120;
+const isFan = false;
+const disccountP = ticketPrice - disccount;
+if (isFan) {
+  console.log(`You are paying${disccountP}`);
+} else {
+  console.log(`You are paying ${ticketPrice}`);
+}
+if ((isFan && userB >= disccountP) || (!isFan && userB >= ticketPrice)) {
+  console.log("You can buy the football ticket");
+} else {
+  console.log("You do not have enough money for the football ticket");
+}
+
+// 1. Online Store Purchase
+// A user is buying a jacket online. The price of the jacket is 250.
+// If it’s a holiday sale, the jacket price is reduced by 50.
+
+// Tasks:
+
+// Store the jacket price in a variable.
+
+// Store the discount amount (50) in another variable.
+
+// Store the user’s balance in another variable.
+
+// Calculate the final jacket price after discount.
+
+// Use a conditional statement to check if the user can buy the jacket:
+
+// If the user’s balance is greater than or equal to the final jacket price, print "You can buy the jacket".
+
+// Otherwise, print "You do not have enough money to buy the jacket".
+
+const jacketPrice = 250;
+const discount = 50;
+const Userb = 300;
+const finalP = jacketPrice - disccount;
+// if (Userb >= finalP) {
+//   console.log("You can buy the jacket");
+// } else {
+//   console.log("You do not have enough money to buy the jacket");
+// }
+
+Userb >= finalP
+  ? console.log("You can buy the jacket")
+  : console.log("You do not have enough money to buy the jacket");
+
+// 2. A student is applying for a driving license. The minimum age required is 16.
+
+// Tasks:
+
+// Store the minimum age requirement in a variable.
+
+// Store the student’s age in another variable.
+
+// Use a conditional statement to check if the student can apply:
+
+// If the student’s age is greater than or equal to the minimum age, print "You are eligible for a driving license".
+
+// Otherwise, print "You are not eligible for a driving license yet".
+const minimumAgeRequired = 16;
+const studentAge = 17;
+// if (studentAge >= minimumAgeRequired) {
+//   console.log("You are eligible for a driving license");
+// } else {
+//   console.log("You are not eligible for a driving license yet");
+// }
+
+studentAge >= minimumAgeRequired
+  ? console.log("You are eligible for a driving license")
+  : console.log("You are not eligible for a driving license yet");
