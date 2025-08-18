97e7b77d2b354991bc184d02a8f320d4b19472a9
shuddy05 <horlabodehyibrahim@gmail.com>
2025-08-18 16:04:35 +0100

    new stuff learnt

--- app.js
+++ app.js
@@ -1,189 +1,46 @@
-//JAVA SCRIPT FUNCTION -
-// reusable blocks of code that perfoms a specific task or return a value
-// call or invove the function
+const africanCountries = ["Nigeria", "Southafrica", "Ghana", "Togo", "Tunisia"];
+console.log(africanCountries.includes("Tanzania"));
+console.log(africanCountries.length);
+africanCountries.unshift("Kenya");
+console.log(africanCountries);
+africanCountries.push("Ethopia");
+console.log(africanCountries);
+africanCountries.sort();
+console.log(africanCountries);
+africanCountries.reverse();
+console.log(africanCountries);
+console.log(africanCountries.concat("China", "India", "Taiwan"));
 
-// a function that welcomes our user when the log in
+const startWithT = africanCountries.find((country) => country.startsWith("T"));
+console.log(startWithT);
 
-function welcomeUser() {
-  console.log("WELCOME USER");
-}
-// calling a function
-welcomeUser();
-welcomeUser();
-// function with parameters - values that are passed to functions
-function welcomeUser2(user) {
-  console.log(`Welcome ${user}`);
-}
+const moreThan5 = africanCountries.filter((country) => country.length > 5);
+console.log(moreThan5);
 
-welcomeUser2("Season");
-welcomeUser2("Daniel");
-welcomeUser2("Ola");
-// deafult parameters
-function welcomeUser3(user = "anonymous") {
-  console.log(`Welcome ${user}`);
-}
-welcomeUser3();
+const toCapitalLetter = africanCountries.map((country) =>
+  country.toUpperCase()
+);
+console.log(toCapitalLetter);
 
-// functions returning a value -  giving back a result
-// return keyword
+const pInIt = africanCountries.every((country) => country.includes("p"));
+console.log(pInIt);
 
-// function that return the additiions of two numbers
-function add(a, b) {
-  return a + b;
-}
-let sum = add(12, 10);
-console.log(sum);
-// function that return the product of two numbers
-function product(numb1, numb2) {
-  return numb1 * numb2;
-}
-console.log(product(65, 3));
-// function that returns the naira equivalent of a dollar amount passed into it
-// 10 dollars will give you (10 * 1600) naira
-// 1$ = 1600
+let myBalance = 5000;
+const transactions = [6000, -3000, 9000, 400, -2000];
 
-function convertToNaira(dollaramount) {
-  return `${dollaramount} dollars will give you ${dollaramount * 1600} Naira`;
-}
-console.log(convertToNaira(22));
-console.log(convertToNaira(40));
+const allCredittransaction = transactions.filter(
+  (transaction) => transaction > 0
+);
+console.log(allCredittransaction);
 
-// write a function the returns the average of three numbers
+const allDebitTransaction = transactions.filter(
+  (transaction) => transaction < 0
+);
+console.log(allDebitTransaction);
+const ifTransactionisGreaterThanBalance = transactions.some(
+  (allCredittransaction) => allCredittransaction > myBalance
+);
+console.log(ifTransactionisGreaterThanBalance);
 
-function calAVG(n1, n2, n3) {
-  const avg = (n1, n2, n3) / 3;
-  return avg;
-}
-console.log(calAVG(12, 13, 14));
-
-function Dob(yob) {
-  return 2025 - yob;
-}
-console.log(Dob(2004));
-
-// write  a functiion that return if a nuber is even or odd
-function oddOrEven(num) {
-  if (num % 2 === 0) {
-    return `${num} is EVEN`;
-  } else {
-    return `${num} is ODD`;
-  }
-}
-console.log(oddOrEven(79));
-console.log(oddOrEven(70));
-
-// write a function to check if a withdraw is possible or not
-
-let balance = 2000;
-function withdrawAmount(amount) {
-  if (balance >= amount) {
-    balance -= amount;
-    return "Transaction Successful";
-  } else {
-    return "Insufficient Funds";
-  }
-}
-
-console.log(withdrawAmount(1800));
-console.log(balance);
-console.log(withdrawAmount(1000));
-
-function depositAmount(amount) {
-  balance += amount;
-  return `Deposit of ${amount} is successful, and your current balane ${balance}`;
-}
-
-console.log(depositAmount(3000));
-console.log(withdrawAmount(1000));
-// RETURN KEYWORD
-// any code after return is inaccessible code
-function example() {
-  console.log("okay after return");
-  return "hello";
-  //unreachable code
-}
-
-function firstLetter(letter) {
-  return letter.charAt(0);
-}
-console.log(firstLetter("daniel"));
-
-function getFull(firstname, lastname) {
-  return firstname + " " + lastname;
-}
-console.log(getFull("Daniel", "Season"));
-
-function longestString(String1, String2) {
-  if (String1.length > String2.length) {
-    return String1;
-  } else if (String2.length > String1.length) {
-    return String2;
-  } else {
-    return "Same length";
-  }
-}
-console.log(longestString("ola", "daniel"));
-
-// function espression, treats function like variable
-
-// function exppression that return the square of a num
-const squareNumber = function (num) {
-  return num ** 2;
-};
-console.log(squareNumber(89));
-
-// function expression that converts number of days to weeks
-const ddaysToWeeks = function (days) {
-  return days / 7;
-};
-
-// arrow function (modern)
-// arrow function that returns the difference between two numbers
-
-const difference2 = (n1, n2) => {
-  const diff = n1 - n2;
-  return diff;
-};
-
-// Shorten arrow functions (works only for one line of code inside the block)
-
-const difference = (n1, n2) => n1 - n2;
-console.log(difference(7, 9));
-
-// HOISTING - behaviour ini JS where function declarations are moved to the top before the code is executed
-hoistedFunc(); // this is working because of hoisting
-function hoistedFunc() {
-  console.log("i am a func dec");
-}
-
-const getMaxNum = (num1, numb2) => {
-  if (num1 > numb2) {
-    return num1;
-  } else if (numb2 > num1) {
-    return numb2;
-  } else {
-    return "Equal number";
-  }
-};
-console.log(getMaxNum(6, 7));
-
-const letter = (string) => string.toUpperCase();
-console.log(letter("daniel"));
-let savings = 50000;
-const checkLoanEligibility = (loanAmount) => {
-  if (savings * 2 < loanAmount) {
-    return "User is Inligible";
-  } else {
-    return "User is Eligible";
-  }
-};
-console.log(checkLoanEligibility(50000));
-
-const checkLoanEligibility1 = (loanAmount) =>
-  savings * 2 < loanAmountN ? "User is Inligible" : "User is Eligible";
-
-const containSubstring = (str, substr) => str.includes(substr);
-console.log(containSubstring("daniel", "nie"));
-
-const convertsMtoS = (nthMinute) => nthMinute * 60;
-console.log(convertsMtoS(120));
+const availableBalance = transactions.reduce((a, b) => a + b, myBalance);
+console.log(availableBalance);
---
+++ array.js
@@ -0,0 +1,125 @@
+// ARRAYS - elements
+// represented using [el1, el2, el3]
+
+const students = ["pious", "daniel", "ola", "season", "shuddy"];
+// array properties
+// length
+
+console.log(students);
+console.log(students.length);
+
+// get element basedon position
+console.log(students[3]);
+
+console.log(students[students.length - 1]); // last element in array
+
+// change element in an array by using the position
+// student[0] = "Zach";
+// student[3] = "emmanuel";
+// console.log(student);
+
+// Array Methods
+// Add or Remove element to/from an array
+// To add element [Push(add to the back)] [unshift(add to the back)]
+
+console.log(students.push("ola"));
+students.push("blessing");
+students.unshift("adee");
+students.pop(); // Remove the End
+students.shift(); // Remove from the front
+console.log(students);
+
+// at , indexOf, lastindexOf
+const nums = ["a", "c", "a", "d", "f", "a", "c"];
+console.log(nums.at(2));
+console.log(nums.indexOf("d"));
+console.log(nums.lastIndexOf("c"));
+
+// Includes
+console.log(nums.includes("a"));
+
+// Extracting portons from an array
+// Slice (start, end(non-inclusive))
+console.log(nums.slice(0, 3));
+
+// Concat
+console.log(students.concat("Jane", "Peter"));
+// Convert Array to String- toString, join(seperator)
+console.log(students.toString());
+console.log(nums.join("-"));
+console.log(nums.toString());
+
+// [] empty array
+console.log(Array.isArray(nums));
+console.log(Array.from("abc"));
+
+// Reverse Array
+const grades = [1, 2, 3];
+console.log(grades);
+console.log(grades.reverse());
+
+// Complex Array Methods
+// Higher Array Methods - because they take another function (call back) to tell them what to do
+// forEach, map, find, sort, filter, every, some, reduce
+
+const transactions = [1000, -200, 3000, -2699, 1000];
+const users = ["ade", "john", "peter", "ayo", "abraham"];
+
+// forEach - allows us to loop through this array - and perfom an action on each of the array
+transactions.forEach((transaction) => {
+  console.log(transaction * 2);
+});
+
+users.forEach((user) => {
+  console.log(user.toUpperCase());
+});
+
+// map - allows us to loop through this array - and perfom an action on each of the array - and return the new array
+const letterFromUsers = users.map((user) => user.charAt(0));
+console.log(letterFromUsers);
+
+const newTransactions = transactions.map((transaction) => transaction / 10);
+console.log(newTransactions);
+
+// find and filter
+// Find- returns the first element that passes a serch conditions
+const bestUser = users.find((user) => user.startsWith("a"));
+console.log(bestUser);
+const greaterThan500 = transactions.find((transaction) => transaction > 500);
+console.log(greaterThan500);
+
+// Filter - returns every elements that passes search condition
+const aUser = users.filter((user) => user.startsWith("a"));
+console.log(aUser);
+
+const debitTransaction = transactions.filter((transaction) => transaction < 0);
+console.log(debitTransaction);
+
+// Sort - arranges element in an array
+console.log(users.sort());
+// When using sort with number, we ned to write a compare function
+const specialNumbers = [11, 2, 3, 4, 17, 22, 3];
+const lowToHigh = specialNumbers.sort((a, b) => a - b);
+console.log(lowToHigh);
+const highToLow = specialNumbers.sort((a, b) => b - a);
+console.log(highToLow);
+
+// every
+const allGreaterThan10 = specialNumbers.every((num) => num > 10);
+console.log(allGreaterThan10);
+
+//some
+const someGreaterThan10 = specialNumbers.some((num) => num > 10);
+console.log(someGreaterThan10);
+
+//  reduce -  accumulate values in an array into one (array of number)
+const cartPrices = [10, 70, 20, 30];
+// a(acc)- accumulator, b(curr)- Current value
+const totalPrice = cartPrices.reduce((acc, curr) => {
+  return acc + curr;
+}, 10);
+console.log(totalPrice);
+
+// Split -
+let myName = "Banana is healthy";
+console.log(myName.split(" "));
